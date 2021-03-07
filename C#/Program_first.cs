31562f0d46bfcec0dcc7807600e57f7cccd47552
Александр Дикин <Dikin@lcoast.itstep>
2021-03-07 11:11:37 +0600

    Первый урок

---
+++ C#/Program_first.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+class Program {
+    static void Main(string[] args) {
+      var arr = new List<int>{0,1,2,3,4,5,6,7};
+
+      foreach(int num in arr){
+        Console.WriteLine(num);
+      }
+    }
+}
+
+
